41554dc7416809f5800a2612f279f3e94759274a
Winothai07x <Winothai.tha@student.mahidol.edu>
2024-11-11 12:21:13 +0700

    Update test_zoo.py

--- test_zoo.py
+++ test_zoo.py
@@ -9,6 +9,22 @@ class TestZoo(unittest.TestCase):
         self.assertEqual(self.zoo.get_ticket_price(5), 50)
        
     # Add your additional test cases here.
+       
+    def test_ticket_price_invalid_age(self):
+        self.assertEqual(self.zoo.get_ticket_price(-3), "Invalided")
+
+    def test_ticket_price_child_boundary(self):
+        self.assertEqual(self.zoo.get_ticket_price(10), 50)
+
+    def test_ticket_price_teen(self):
+        self.assertEqual(self.zoo.get_ticket_price(15), 100)
+
+    def test_ticket_price_adult(self):
+        self.assertEqual(self.zoo.get_ticket_price(40), 150)
+
+    def test_ticket_price_senior(self):
+        self.assertEqual(self.zoo.get_ticket_price(70), 100)
+
 
 if __name__ == '__main__':
     unittest.main()
